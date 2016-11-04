6ec4f0ec9a5679bc37bfb52606dd8b9a73d21ef7
Heisenbean <hedongbin1991@gmail.com>
2016-11-04 17:55:34 +0800

    add READEME

--- t66ySpider/t66y.py
+++ t66ySpider/t66y.py
@@ -6,7 +6,7 @@ import re
 import urllib2
 import requests
 
-site= "http://t66y.com/htm_data/7/1611/2124550.html"
+site= raw_input('请输入地址:')
 header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
